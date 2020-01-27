1c5ac7562d36837024f6ed9af4f5fc36fcd75420
VATOC <60084696+VATOC@users.noreply.github.com>
2020-01-26 22:31:48 -0600

    sixth response

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import requests
+import json
 
 if __name__ == '__main__':
-    url= 'https://www.google.com/'
-    response = requests.get(url)
+    url= 'https://httpbin.org/post'
+    payload = {'nombre':'Roberto','curso':'python', 'nivel':'intermedio'}
 
+    response = requests.post(url, data= json.dumps(payload) )
+    #print(response.url)
     #print(response)
     if response.status_code == 200:
         #print(response.content)
-        content = response.content
-        file = open('google.html', 'wb')
-        file.write(content)
-        file.close()
+        #content = response.content
+        #response_json = response.json() #dict
+        #origin = response_json['origin']
+        #print(origin)
+        #print(content)
+        #file = open('google.html', 'wb')
+        #file.write(content)
+        #file.close()
+        #response_json = json.loads(response.text)
+        #origin = response_json['origin']
+        #print(origin)
+        print(response.content)
